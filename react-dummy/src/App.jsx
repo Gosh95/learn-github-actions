8e96341ba60b96f566504a24deff9a5dc8d1d4a5
Gosh95 <pocal777@naver.com>
2023-05-13 23:01:50 +0900

    워크 플로우 실행 스킵 [skip ci]

--- react-dummy/src/App.jsx
+++ react-dummy/src/App.jsx
@@ -1,6 +1,8 @@
 import MainContent from './components/MainContent';
 import logo from './assets/images/logo.png';
 
+//커밋 메시지 뒤에 [skip ci], [ci skip] 등을 추가하면 워크 플로우 실행을 스킵한다.
+
 function App() {
   return (
     <>
